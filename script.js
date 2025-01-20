d1da89fc46b0a29cc7aaa877ff57a12ca61c712f
tguigue30 <theo.guigue@gmail.com>
2025-01-20 10:02:28 +0100

    Correction d'erreurs sur la mise en forme suite à la supression d'un boutton.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const githubButton = document.getElementById("githubButton");
-  const coursButton = document.getElementById("coursButton");
-  const PEAButton = document.getElementById("PEAButton");
   const image = document.getElementById("logoImage");
   const currentTimeElement = document.getElementById("currentTime");
   const h1Element = document.querySelector("h1");
@@ -20,8 +18,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
   }
 
   addHoverEffect(githubButton);
-  addHoverEffect(coursButton);
-  addHoverEffect(PEAButton);
 
   image.addEventListener("mouseover", () => {
     image.style.transform = "scale(1.1)";
